777613708fc2e5e492962b29cc9db395d394b569
dOsan3 <>
2023-08-27 01:00:08 -0400

    Smarter calculations and error messages for token limits.

--- modules/find_info.py
+++ modules/find_info.py
@@ -2,9 +2,10 @@ import os
 import json
 import configparser
 from .token_checker import check_token_length
+from .token_counter import num_tokens_from_string
 from .llm_model import generate_completion
 
-async def find_relevant_info(user_prompt, max_token_length=3900):
+async def find_relevant_info(user_prompt, max_token_length=8192):
     # Read config file.
     config = configparser.ConfigParser()
     config.read('filebot.config')
@@ -37,7 +38,7 @@ def get_file_content(file_path):
 
     return content
 
-async def answer_prompt(file_path, user_prompt, max_token_length=3900, answer_type="non_final_answer"):
+async def answer_prompt(file_path, user_prompt, max_token_length=8192, answer_type="non_final_answer"):
     content = get_file_content(file_path)
 
     config = configparser.ConfigParser()
@@ -49,10 +50,18 @@ async def answer_prompt(file_path, user_prompt, max_token_length=3900, answer_ty
     else:
         prepend_prompt = config['DEFAULT'].get('PrependPrompt', '')
 
-    is_within_limit, user_prompt = check_token_length(user_prompt, max_token_length, 'gpt-3')
+    # Check if final_prompt (less user_prompt) exceeds token limit
+    final_prompt_without_user_prompt = f"{prepend_prompt}. based on the following: ```{content}```"
+    final_prompt_without_user_prompt_token_count = num_tokens_from_string(final_prompt_without_user_prompt, 'gpt-3')
+    if final_prompt_without_user_prompt_token_count > max_token_length:
+        excess_tokens = final_prompt_without_user_prompt_token_count - max_token_length
+        return f"Content filebot needs to retrieve exceeds the token limit by {excess_tokens}."
 
-    if not is_within_limit:
-        return "The content is too large to summarize."
+    # Check if there are excess tokens available for user_prompt
+    excess_tokens = max_token_length - final_prompt_without_user_prompt_token_count
+    user_prompt_token_count = num_tokens_from_string(user_prompt, 'gpt-3')
+    if user_prompt_token_count > excess_tokens:
+        return f"Your query is consuming too many tokens ({user_prompt_token_count}). Reduce your prompt by {user_prompt_token_count - excess_tokens} tokens, please."
 
     final_prompt = f"{prepend_prompt}. {user_prompt}, based on the following: ```{content}```"
 
